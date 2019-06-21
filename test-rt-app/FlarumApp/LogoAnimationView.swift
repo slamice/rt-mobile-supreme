92d6a0afd4cd470c14e6acf1cc2ded0e66f8842f
Luis Sanmiguel <luis@rocketrip.com>
2019-06-21 17:36:19 -0400

    Please work

---
+++ test-rt-app/FlarumApp/LogoAnimationView.swift
@@ -0,0 +1,9 @@
+//
+//  LogoAnimationView.swift
+//  FlarumApp
+//
+//  Created by Luis Sanmiguel on 6/21/19.
+//  Copyright © 2019 Chamath Palihawadana. All rights reserved.
+//
+
+import Foundation
