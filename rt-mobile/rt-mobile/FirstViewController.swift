914bd842254328a0867f7f08622510fe6397294c
slamice <slamice@gmail.com>
2019-06-21 10:15:52 -0400

    added new project

---
+++ rt-mobile/rt-mobile/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  rt-mobile
+//
+//  Created by Issam Zeibak on 6/21/19.
+//  Copyright © 2019 Issam Zeibak. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
